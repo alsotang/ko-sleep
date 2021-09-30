04a36ec131c15ab5e762074f6f182b1bbea50648
alsotang <alsotang@gmail.com>
2021-09-30 19:30:50 +0800

    add jsdoc

--- src/sleep.ts
+++ src/sleep.ts
@@ -1,5 +1,8 @@
 import ms from 'ms'
 
+/**
+ * @param time - millisecond number, or '1s' which https://www.npmjs.com/package/ms support
+ */
 function sleep(time: number | string): Promise<void> {
   let numTime: number
   if (typeof time === 'string'){
